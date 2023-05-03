6313847babd440fa9124af41c6e479d3a1d1ae9b
Jim Burbridge <jhechtf@gmail.com>
2023-05-03 14:10:02 -0700

    deps: Adds in support for React

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import { resolve } from 'node:path';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue(),
-    tailwind()
-  ],
+  integrations: [vue(), tailwind(), react()],
   vite: {
     resolve: {
       alias: {
